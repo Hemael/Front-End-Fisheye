6bc48937220c4d2719dc55038649b1ffd6a4812b
unknown <chloe.legouas@hotmail.fr>
2022-08-08 15:14:17 +0200

    changement majeur

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,14 @@
     async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+
+        //recuperer les donnés de data
+        const response = await fetch('/data/photographers.json');
+        const photographe = await response.json();
+        // retourner photographers
+        return photographe.photographers;
     }
+
+
+
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
@@ -35,9 +20,11 @@
         });
     };
 
+
+
     async function init() {
         // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
+        const  photographers  = await getPhotographers();
         displayData(photographers);
     };
     
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,30 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+
+async function getMedia(photographerId) {
+
+    //recuperer les donnés de data
+    const response = await fetch('/data/photographers.json');
+    const photographe = await response.json();
+    // retourner photographers
+    return photographe.media.filter(vaba  => vaba.photographerId == photographerId)
+    
+    
+}
+
+
+
+
+async function displayData(medias) {
+    console.log(medias);
+};
+
+
+
+async function init() {
+    // Récupère les datas des photographes
+    const  medias  = await getMedia(82);
+    displayData(medias);
+};
+
+init();
